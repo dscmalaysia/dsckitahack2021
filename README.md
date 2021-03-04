b8434131bd26dd0a5c9ff4abacd687fe9d5d0ba6
Tech-99 <takieddine@users.noreply.github.com>
2021-03-04 23:34:56 +0800

    Please read the README file for the report

--- README.md
+++ README.md
@@ -1,22 +1,11 @@
-# index.html merging process
-
-**_MERGING PROCESS BEGAN 03/03_**
+# Latest update
 
 ## Key notes
 
-1.  All CSS files merged into one single CSS file `main.css` duplicates are not yet removed, we need to meet and talk about them
-2.  The `index.html` file is not fully merged, some parts are still missing and some are not made responsive yet. Need the help of the team to make them so.
-3.  Imported the pictures from Figma as SVGs.
-4.  Added relevant comments to make the code easier to read and review
+1. Added the copywriting provided by Kwong and Dani
+2. Fixed the positioning of the "+" of the FAQ
 
 ## What's next
 
-1.  Review the code for feedback and comments.
-2.  Bug fixing: Responsiveness mainly and some other key features to fix.
-3.  Work on the missing stuff (mainly me taki the writer of this as of now FAQ + SCHEDULE)
-
-**NOTE: I kept a backup file this time (even during the writing of this I messed up several times and lost changes because of branch changing XD don't blast me on dis pls it hurts to write code literally thrice :'( but I do have a backup file with all of the changes so no worries)**
-
-**Feedback is most welcome ping me on discord**
-
-**_CHEERS LADIES AND FELLERS_**
+1. Waiting for more content to be added to the website and the registration link
+   **_CHEERS LADIES AND FELLERS_**
--- js/FAQ.js
+++ js/FAQ.js
@@ -1,7 +1,38 @@
 let FAQs = [
-  ['Question1', 'answer1'],
-  ['Question2', 'answer2'],
+  [
+    "I am a beginner, can I participate?",
+    "Sure! Anyone with any background can participate. Even ur waifu also can come.",
+  ],
+  [
+    "What is a hackathon? Can I hack Google?",
+    "A hackathon is a design sprint-like event; often, in which computer programmers and others involved in software development, including graphic designers, interface designers, project managers, domain experts, and others collaborate intensively on software projects. If you manage to hack google, do tell them here for a bounty: https://www.google.com/about/appsecurity/ ",
+  ],
+  [
+    "Do I have to form a team to participate?",
+    "Not at all! If you prefer to run solo, feel free to register as an individual team in our Google Form! If you prefer to participate with a group of your friends, you can participate with up to 4 of your friends in a team!",
+  ],
+  ["Can I solo team then?", "You can. But what is the fun in it then? 🥺"],
+  [
+    "What do I have to hack/solve for this hackathon?",
+    "The theme of this hackathon revolves around the 17 UN Sustainable Development Goals (which can be found @ https://sdgs.un.org/goals). Teams are tasked to <b>solve for one or more</b> of the United Nations 17 Sustainable Development Goals using Google technology.",
+  ],
+  [
+    "What are the SDGs?",
+    'The Sustainable Development Goals or Global Goals are a collection of 17 interlinked global goals designed to be a "blueprint to achieve a better and more sustainable future for all". The SDGs were set in 2015 by the United Nations General Assembly and are intended to be achieved by the year 2030',
+  ],
+  [
+    "Soo… What happens after the hackathon?",
+    "You can refine your solutions, and participate in the 2021 Solution Challenge! The Solution Challenge is an annual global hackathon by Google. Win prizes and a chance to connect with the C-Levels of Google itself!",
+  ],
+  [
+    "What is the Solution Challenge?",
+    "The 2021 Solution Challenge mission is to solve for one or more of the United Nations 17 Sustainable Development Goals using Google technology. You can view more details here: https://developers.google.com/community/dsc-solution-challenge",
+  ],
+  [
+    "Who owns the idea / solution?",
+    "You, and only you! We don’t take any credit / royalties or any of the sort from you / your solution.",
+  ],
 ];
-let QUESTION = '',
-  ANSWER = '';
-let section = document.getElementById('FAQ-WRAPPER');
+let QUESTION = "",
+  ANSWER = "";
+let section = document.getElementById("FAQ-WRAPPER");
