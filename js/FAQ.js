5682f0cc848839823fd68e2ee9eed8d9d6a18b04
Kim Sheng Yong <kimshengyong@gmail.com>
2021-03-05 14:23:06 +0800

    update mentors and judges

--- js/FAQ.js
+++ js/FAQ.js
@@ -1,38 +1,51 @@
 let FAQs = [
   [
-    "I am a beginner, can I participate?",
-    "Sure! Anyone with any background can participate. Even ur waifu also can come.",
+    'I am a beginner, can I participate?',
+    'Sure! Anyone with any background can participate. Even ur waifu also can come.',
   ],
   [
-    "What is a hackathon? Can I hack Google?",
-    "A hackathon is a design sprint-like event; often, in which computer programmers and others involved in software development, including graphic designers, interface designers, project managers, domain experts, and others collaborate intensively on software projects. If you manage to hack google, do tell them here for a bounty: https://www.google.com/about/appsecurity/ ",
+    'What is a hackathon? Can I hack Google?',
+    'A hackathon is a design sprint-like event; often, in which computer programmers and others involved in software development, including graphic designers, interface designers, project managers, domain experts, and others collaborate intensively on software projects. If you manage to hack google, do tell them here for a bounty: https://www.google.com/about/appsecurity/ ',
   ],
   [
-    "Do I have to form a team to participate?",
-    "Not at all! If you prefer to run solo, feel free to register as an individual team in our Google Form! If you prefer to participate with a group of your friends, you can participate with up to 4 of your friends in a team!",
+    'Do I have to form a team to participate?',
+    'Not at all! If you prefer to run solo, feel free to register as an individual team in our Google Form! If you prefer to participate with a group of your friends, you can participate with up to 4 of your friends in a team!',
   ],
-  ["Can I solo team then?", "You can. But what is the fun in it then? 🥺"],
+  ['Can I solo team then?', 'You can. But what is the fun in it then? 🥺'],
   [
-    "What do I have to hack/solve for this hackathon?",
-    "The theme of this hackathon revolves around the 17 UN Sustainable Development Goals (which can be found @ https://sdgs.un.org/goals). Teams are tasked to <b>solve for one or more</b> of the United Nations 17 Sustainable Development Goals using Google technology.",
+    'What do I have to hack/solve for this hackathon?',
+    'The theme of this hackathon revolves around the 17 UN Sustainable Development Goals (which can be found @ https://sdgs.un.org/goals). Teams are tasked to <b>solve for one or more</b> of the United Nations 17 Sustainable Development Goals using Google technology.',
   ],
   [
-    "What are the SDGs?",
+    'What are the SDGs?',
     'The Sustainable Development Goals or Global Goals are a collection of 17 interlinked global goals designed to be a "blueprint to achieve a better and more sustainable future for all". The SDGs were set in 2015 by the United Nations General Assembly and are intended to be achieved by the year 2030',
   ],
   [
-    "Soo… What happens after the hackathon?",
-    "You can refine your solutions, and participate in the 2021 Solution Challenge! The Solution Challenge is an annual global hackathon by Google. Win prizes and a chance to connect with the C-Levels of Google itself!",
+    'Soo… What happens after the hackathon?',
+    'You can refine your solutions, and participate in the 2021 Solution Challenge! The Solution Challenge is an annual global hackathon by Google. Win prizes and a chance to connect with the C-Levels of Google itself!',
   ],
   [
-    "What is the Solution Challenge?",
-    "The 2021 Solution Challenge mission is to solve for one or more of the United Nations 17 Sustainable Development Goals using Google technology. You can view more details here: https://developers.google.com/community/dsc-solution-challenge",
+    'What is the Solution Challenge?',
+    'The 2021 Solution Challenge mission is to solve for one or more of the United Nations 17 Sustainable Development Goals using Google technology. You can view more details here: https://developers.google.com/community/dsc-solution-challenge',
   ],
   [
-    "Who owns the idea / solution?",
-    "You, and only you! We don’t take any credit / royalties or any of the sort from you / your solution.",
+    'Who owns the idea / solution?',
+    'You, and only you! We don’t take any credit / royalties or any of the sort from you / your solution.',
+  ],
+  [
+    'How is the evaluation criteria?',
+    '<h3><strong>Impact</strong></h3><li>Does the entry establish a clear challenge focused on one or more of the United Nations 17 Sustainable Development Goals? Is it explained in a clear manner with specific Sustainable Development Goal targets they are looking to solve for? [10]</li><li>How effectively does the solution address the challenge identified by the team? [20]</li><li>Is there evidence of a next step? Does the team display a plan for future extension if they were to continue? [10]</li><h3><strong>Technology</strong></h3><li>Has the team clearly explained what Google technology they used, why, and included guidance on how to run their code? [10]</li><li>Has the team made effective product and technical decisions for their solution? [10]</li>',
   ],
 ];
-let QUESTION = "",
-  ANSWER = "";
-let section = document.getElementById("FAQ-WRAPPER");
+let QUESTION = '',
+  ANSWER = '';
+let section = document.getElementById('FAQ-WRAPPER');
+{
+  /* <strong>Impact</strong>
+<li>Does the entry establish a clear challenge focused on one or more of the United Nations 17 Sustainable Development Goals? Is it explained in a clear manner with specific Sustainable Development Goal targets they are looking to solve for? [10]</li>
+<li>How effectively does the solution address the challenge identified by the team? [20]</li>
+<li>Is there evidence of a next step? Does the team display a plan for future extension if they were to continue? [10]</li>
+<strong>Technology</strong>
+<li>Has the team clearly explained what Google technology they used, why, and included guidance on how to run their code? [10]</li>
+<li>Has the team made effective product and technical decisions for their solution? [10]</li> */
+}
